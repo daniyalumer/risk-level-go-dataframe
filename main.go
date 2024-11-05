77e0d03c606d09f8f6c88bd747644fc01575ba39
daniyalumer <daniyalharal100@gmail.com>
2024-11-05 15:56:29 +0500

    Fixing Output File Name

--- main.go
+++ main.go
@@ -4,36 +4,33 @@ import (
 	"flag"
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/daniyalumer/csvdataframe/risklevel"
 )
 
 func main() {
 	inputFile := flag.String("input", "", "Input CSV file path (required)")
-	outputPath := flag.String("output", "", "Output directory path (required)")
+	outputFile := flag.String("output", "", "Output CSV file path (required)")
 	flag.Parse()
 
-	if *inputFile == "" || *outputPath == "" {
+	if *inputFile == "" || *outputFile == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	path, fileName := filepath.Split(*inputFile)
-
-	log.Printf("Looking for CSV file: %s in directory: %s\n", fileName, path)
+	log.Printf("Looking for CSV file: %s\n", *inputFile)
 
 	if _, err := os.Stat(*inputFile); err != nil {
 		if os.IsNotExist(err) {
-			log.Printf("Error: CSV file not found: %s\n", fileName)
+			log.Printf("Error: CSV file not found %s\n", *inputFile)
 		} else {
-			log.Printf("Error accessing file %s: %v\n", fileName, err)
+			log.Printf("Error accessing file %s\n", *inputFile)
 		}
 		return
 	}
 
-	risklevel.RiskLevelAssessment(path, fileName, *outputPath)
+	risklevel.RiskLevelAssessment(*inputFile, *outputFile)
 
-	log.Printf("Processing complete. Output saved to %s\n", *outputPath)
+	log.Printf("Processing complete. Output saved to %s\n", *outputFile)
 
 }
--- risklevel/risklevel.go
+++ risklevel/risklevel.go
@@ -2,23 +2,27 @@ package risklevel
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
 	dataframe "github.com/datumbrain/go-dataframe"
 )
 
-func RiskLevelAssessment(path string, csvName string, outputPath string) {
-	df := dataframe.CreateDataFrame(path, csvName)
+func RiskLevelAssessment(inputPath string, outputFile string) {
+	inputPath, inputFileName := filepath.Split(inputPath)
+
+	df := dataframe.CreateDataFrame(inputPath, inputFileName)
 	log.Println("Processing data...")
 
 	df.NewField("Risk Level")
 	for _, row := range df.FrameRecords {
 		score := row.ConvertToInt("Score", df.Headers)
 		switch {
-		case score >= 0 && score < 4:
+		case score >= 0 && score <= 4:
 			row.Update("Risk Level", "High", df.Headers)
-		case score >= 4 && score < 7:
+		case score >= 4 && score <= 7:
 			row.Update("Risk Level", "Medium", df.Headers)
-		case score >= 7 && score < 9:
+		case score >= 7 && score <= 9:
 			row.Update("Risk Level", "Low", df.Headers)
 		case score == 10:
 			row.Update("Risk Level", "No Risk", df.Headers)
@@ -26,7 +30,19 @@ func RiskLevelAssessment(path string, csvName string, outputPath string) {
 	}
 
 	df.Sort("Risk Level")
-	if !df.SaveDataFrame(outputPath, "page-stats-aggregator") {
+
+	outputPath, outputFileName := filepath.Split(outputFile)
+	if _, err := os.Stat(outputPath); err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("Error: Output directory does not exist: %s\n", outputPath)
+			return
+		}
+		log.Printf("Error accessing output directory: %s\n", outputPath)
+		return
+	}
+
+	log.Printf("Saving output to %s\n", outputPath)
+	if !df.SaveDataFrame(outputPath, outputFileName) {
 		log.Printf("Error: failed to save CSV to output path: %s\n", outputPath)
 		return
 	}
